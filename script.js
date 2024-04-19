73005e82bec91f84a3974a5439855b648482dff3
tactik8 <48621231+tactik8@users.noreply.github.com>
2024-04-19 18:39:03 +0000

    go

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ import { KrThing } from './kraken_record/kraken_record.js';
 
 
 
+function test(event){
+    console.log('ok', event)
+}
+
 let k = new KrThing()
 console.log('z', k)
+
+k.register(test)
+
+k.setProperty('name', 'bob')

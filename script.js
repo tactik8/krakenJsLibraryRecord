5d95fbcb31e2258a9c1dd03d66bd96db4a03fd63
tactik8 <48621231+tactik8@users.noreply.github.com>
2024-05-15 18:19:09 +0000

    go

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 import { KrThing } from './kraken_record/kraken_record.js';
 
 
+function test(){
 
-let k = new KrThing('Thing', 'bob')
-k.setProperty('name', 'bob2')
-console.log('vv', k.getSystemRecord())
-k.api_post()
+    
+    
+    let k = new KrThing('Thing', 'bob')
+    k.setProperty('name', 'bob2')
+    console.log('vv', k.getSystemRecord())
+    k.api_post()
+    
+    
+    let k2 =  new KrThing('Thing', 'bob')
+    k2.api_get()
+    console.log('ww', k2.record)
 
-
-let k2 =  new KrThing('Thing', 'bob')
-k2.api_get()
-console.log('ww', k2.record)
+}
